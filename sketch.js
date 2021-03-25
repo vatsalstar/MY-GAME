e388fe6c7505c0ba698c3f7b19668cd679682760
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2021-03-25 15:43:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 var boy,backgroundImg,boyImg,back;
 var ground,car,bike,power,carImg,bikeImg,powerImg;
+var carGroup,bikeGroup;
+var gameState = "Play";
+var Gameover;
+var jump,go,back;
+var score;
 
 function preload() {
     backgroundImg = loadImage("sprites/BG.png");
-    boyImg = loadImage("sprites/BOY.png");
-    carImg = loadImage("sprites/car.jpg");
-    bikeImg = loadImage("sprites/bike.jpg");
+    boyImg = loadImage("sprites/boy.png");
+    carImg = loadImage("sprites/car.png");
+    bikeImg = loadImage("sprites/bike.png");
+    Gameover = loadImage("sprites/Gameover.jpg");
+    jump = loadSound("jump.mp3");
+    go = loadSound("gameover.mp3");
+    back = loadSound("back.mp3");
 }
 
 function setup(){
@@ -16,12 +25,16 @@ function setup(){
 
     boy = createSprite(70,580);
     boy.addImage(boyImg);
-    boy.scale =0.5;
+    boy.scale =0.3;
 
 
     ground = createSprite(600,690,1200,20);
     ground.visible=false;
 
+    carGroup = createGroup();
+    bikeGroup = createGroup();
+
+    score = 0;
     
 }
 
@@ -29,42 +42,82 @@ function setup(){
 
 function draw(){
     background("black");
+    back.loop;
 
+    if (gameState === "Play"){
+
+        score=score+Math.round(frameCount/100);
+      
     if(back.x<500){
         back.x = 600;
     }
-    back.velocityX = -6;
+    back.velocityX = -4;
 
-    if(keyDown("space")){
+
+    if(boy.x>1150){
+        boy.x=70;
+
+    }
+    boy.velocityX = 4;
+
+    if(keyDown("space")&&boy.y>=400){
     boy.velocityY = -10;
+    jump.play();
     }
     boy.velocityY=boy.velocityY+0.8;
-
-    boy.collide(ground);
-
+    
     spawnCars();
     spawnBike();
 
+    if (carGroup.isTouching(boy)||bikeGroup.isTouching(boy)){
+        gameState = "End";
+        go.play();
+    }
+  
+    }
+
+    else if(gameState==="End"){
+        back.velocityX=0;
+        boy.velocityY=0;
+        back.visible=false;
+        boy.visible=false;
+        carGroup.setVelocityXEach(0);
+        bikeGroup.setVelocityXEach(0);
+        carGroup.destroyEach();
+        bikeGroup.destroyEach();
+
+        image(Gameover,0,0,1200,700);
+
+    }
+
+    boy.collide(ground);
+
     drawSprites();
+
+    fill("white");
+    textSize(40);
+    text("SCORE:"+score,900,50);
 
 }
 
 function spawnCars(){
-    if(frameCount%180===0){
+    if(frameCount%100===0){
         car = createSprite(1200,650);
     car.addImage(carImg);
     car.scale = 0.5;
     car.velocityX = -6;
+    carGroup.add(car);
     }
 }
 
 
 function spawnBike(){
-    if(frameCount%280===0){
+    if(frameCount%200===0){
         bike = createSprite(1200,650);
     bike.addImage(bikeImg);
-     bike.scale = 0.5;
+     bike.scale = 0.3;
     bike.velocityX = -6;
+    bikeGroup.add(bike);
     }
 }
 
